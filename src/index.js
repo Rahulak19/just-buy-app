1b5ad64b5d2914b32fd3e43a055779a1af8e046a
Rahulak19 <rahuladhi2011@gmail.com>
2023-09-17 02:34:25 +0530

    Added logs

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ if(process.env.ENV_PATH)
     const appContainer = require("./config/container").init();
     require("./config/DbContainer").init(appContainer).then(data =>{
     if(data){
+        console.log("before pre")
         appContainer.resolve("apiRoute").preHandler();
+        console.log("before path")
         appContainer.resolve("apiRoute").pathDefination(appContainer);
+        console.log("before pos")
         appContainer.resolve("apiRoute").postHandler();
+        console.log("before server")
         appContainer.resolve("server").start(appContainer);
     }
     else{
