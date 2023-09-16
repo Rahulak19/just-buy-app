7503e9ad8140d20b7001d9a225dfd20336d5c021
Rahulak19 <rahuladhi2011@gmail.com>
2023-09-17 02:29:32 +0530

    modified server.js file

--- src/config/server.js
+++ src/config/server.js
@@ -2,14 +2,28 @@ module.exports=({https,http,app,logger})=>{
     return{
         start:(container)=>{
             try{
-                const server = http.createServer(app)
-                server.listen(process.env.PORT,()=>{
-                    logger.info(`Application started at port ${process.env.PORT}`);
+                if(process.env.ENV==="local")
+                {
+                    const server = http.createServer(app)
+                    server.listen(process.env.PORT,()=>{
+                    logger.info(`Application started at port http ${process.env.PORT}`);
                 })
-                server.on('close',()=>{
+                    server.on('close',()=>{
                     logger.info(`Server stopped`);
                     container.dispose();
                 })
+                }
+                else{
+                    const server = https.createServer(app)
+                    server.listen(process.env.PORT,()=>{
+                    logger.info(`Application started at port https ${process.env.PORT}`);
+                })
+                    server.on('close',()=>{
+                    logger.info(`Server stopped`);
+                    container.dispose();
+                })
+                }
+                
             }
             catch(error){
                 logger.error(`Error Occurred while starting the server ${error}`);
