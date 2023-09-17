370e6911d2abb57257f01473e663a7c031afe38f
Rahulak19 <rahuladhi2011@gmail.com>
2023-09-17 11:36:42 +0530

    updated port

--- src/config/server.js
+++ src/config/server.js
@@ -18,11 +18,11 @@ module.exports=({https,http,app,logger})=>{
                 }
                 else{
                    
-                    const server = https.createServer(app)
+                    const server = http.createServer(app)
                   
                     const host = '0.0.0.0'; 
                     server.listen(process.env.PORT,host,()=>{
-                    logger.info(`Application started at port https ${process.env.PORT}`);
+                    logger.info(`Application started at port http ${process.env.PORT}`);
                 })
                     server.on('close',()=>{
                     logger.info(`Server stopped`);
